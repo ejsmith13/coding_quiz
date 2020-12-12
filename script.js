335484794721b9b964c481bbaccd3f279f7dbd05
ejsmith13 <ejsmithwork@gmail.com>
2020-12-12 07:44:35 -0500

    alignment fix, add answers to buttons

--- script.js
+++ script.js
@@ -2,56 +2,74 @@ var startQuiz = document.querySelector(".quiz");
 var startBtn = document.querySelector("#start");
 var scores = document.querySelector(".scoreList ");
 var timeLeft = document.querySelector(".timer");
-var mainSection= document.querySelector(".main");
+var mainSection = document.querySelector(".main");
 var questionOne = document.createElement("div");
-var answersOne = document.createElement("button");
-var answerTwo = document.createElement("button");
-var answerThree = document.createElement("button");
-var answerFour = document.createElement("button");
+var buttonOne = document.createElement("button");
+var buttonTwo = document.createElement("button");
+var buttonThree = document.createElement("button");
+var buttonFour = document.createElement("button");
+var answerOne = document.createElement("p");
+var answerTwo = document.createElement("p");
+var answerThree = document.createElement("p");
+var answerFour = document.createElement("p");
 
 var secondsLeft = 90;
+var score = 0;
 
 var button = document.createElement("button");
 
-function startDisappear () {
-    console.log("Starting Dissappear");
-    startQuiz.setAttribute("style", "display: none");
-    startTimer();
+function startDisappear() {
+  console.log("Starting Dissappear");
+  startQuiz.setAttribute("style", "display: none");
+  startTimer();
 }
 
-function startTimer (){
-    console.log("starting Timer");
+function startTimer() {
+  console.log("starting Timer");
 
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeLeft.textContent = "Time: " + secondsLeft;
-    
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          enterScore();
-        }
-    
-      }, 1000);
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeLeft.textContent = "Time: " + secondsLeft;
 
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      enterScore();
+    }
+  }, 1000);
 
-    startQ ();
+  startQ();
 }
 
-function startQ(){
-    console.log("startingQuiz");
+function startQ() {
+  console.log("startingQuiz");
 
-    questionOne.textContent = "This is an example question; Answer 3"
-    
-    mainSection.appendChild(questionOne);
+  questionOne.textContent = "This is an example question; button 3";
+
+  buttonOne.textContent = "Answer 1";
+  buttonTwo.textContent = "Answer 2";
+  buttonThree.textContent = "Answer 3";
+  buttonFour.textContent = "Answer 4";
+
+  answerOne.textContent = "example";
+  answerTwo.textContent = "example";
+  answerThree.textContent = "example";
+  answerFour.textContent = "example";
 
 
+  mainSection.appendChild(questionOne);
 
+  mainSection.appendChild(buttonOne);
+  mainSection.appendChild(answerOne);
+  mainSection.appendChild(buttonTwo);
+  mainSection.appendChild(answerTwo);
+  mainSection.appendChild(buttonThree);
+  mainSection.appendChild(answerThree);
+  mainSection.appendChild(buttonFour);
+  mainSection.appendChild(answerFour);
 }
 
 function enterScore() {
-    console.log("enterscore!");
-
+  console.log("enterscore!");
 }
 
-
-startBtn.addEventListener("click", startDisappear)
+startBtn.addEventListener("click", startDisappear);
