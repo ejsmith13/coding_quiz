0fcc8b448b220835c25900da246b9e436e8a4ae1
ejsmith13 <ejsmithwork@gmail.com>
2020-12-13 16:28:58 -0500

    added and removed buttons

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ var buttonTwo = document.createElement("button");
 var buttonThree = document.createElement("button");
 var buttonFour = document.createElement("button");
 
+var buttonFive = document.createElement("button");
+var buttonSix = document.createElement("button");
+var buttonSeven = document.createElement("button");
+var buttonEight = document.createElement("button");
+
+var button9 = document.createElement("button");
+var button10 = document.createElement("button");
+var button11 = document.createElement("button");
+var button12 = document.createElement("button");
+
+var button13 = document.createElement("button");
+var button14 = document.createElement("button");
+var button15 = document.createElement("button");
+var button16 = document.createElement("button");
+
+var button17 = document.createElement("button");
+var button18 = document.createElement("button");
+var button19 = document.createElement("button");
+var button20 = document.createElement("button");
+
 var answerOne = document.createElement("p");
 var answerTwo = document.createElement("p");
 var answerThree = document.createElement("p");
@@ -32,10 +52,10 @@ function startTimer() {
     secondsLeft--;
     timeLeft.textContent = "Time: " + secondsLeft;
 
-    if (secondsLeft === 0 || secondsLeft < 0) {
-      clearInterval(timerInterval);
-      enterScore();
-    }
+    // if (secondsLeft === 0 || secondsLeft < 0) {
+    //   clearInterval(timerInterval);
+    //   enterScore();
+    // }
   }, 1000);
 
   startQ();
@@ -43,6 +63,8 @@ function startTimer() {
 
 function startQ() {
   console.log("startingQuiz");
+
+  
 
   questionOne.textContent = "Commonly used data types DO NOT include:";
 
@@ -56,85 +78,108 @@ function startQ() {
   answerThree.textContent = "Alerts";
   answerFour.textContent = "Numbers";
 
-  mainSection.appendChild(questionOne);
+  mainSection.append(questionOne);
 
-  mainSection.appendChild(buttonOne);
-  mainSection.appendChild(answerOne);
-  mainSection.appendChild(buttonTwo);
-  mainSection.appendChild(answerTwo);
-  mainSection.appendChild(buttonThree);
-  mainSection.appendChild(answerThree);
-  mainSection.appendChild(buttonFour);
-  mainSection.appendChild(answerFour);
+  mainSection.append(buttonOne);
+  mainSection.append(answerOne);
+  mainSection.append(buttonTwo);
+  mainSection.append(answerTwo);
+  mainSection.append(buttonThree);
+  mainSection.append(answerThree);
+  mainSection.append(buttonFour);
+  mainSection.append(answerFour);
 
   buttonOne.addEventListener("click", takeTime);
   buttonTwo.addEventListener("click", takeTime);
   buttonThree.addEventListener("click", addPoint);
   buttonFour.addEventListener("click", takeTime);
+  return score
+
 
   function addPoint() {
     score = score + 10;
     console.log(score);
     startTwo();
+    return score
   }
 
   function takeTime() {
     secondsLeft = secondsLeft - 10;
     startTwo();
+    return score
   }
 }
 
 function startTwo() {
     console.log("starting2");
 
+    
+
     questionOne.textContent = "A boolean has how many possible values?";
+
+    buttonOne.remove();  
+    buttonTwo.remove(); 
+    buttonThree.remove(); 
+    buttonFour.remove();
   
-    buttonOne.textContent = "A";
-    buttonTwo.textContent = "B";
-    buttonThree.textContent = "C";
-    buttonFour.textContent = "D";
+    buttonFive.textContent = "A";
+    buttonSix.textContent = "B";
+    buttonSeven.textContent = "C";
+    buttonEight.textContent = "D";
   
     answerOne.textContent = "1";
     answerTwo.textContent = "2";
     answerThree.textContent = "10";
     answerFour.textContent = "infinate";
   
-    mainSection.appendChild(questionOne);
+    mainSection.append(questionOne);
   
-    mainSection.appendChild(buttonOne);
-    mainSection.appendChild(answerOne);
-    mainSection.appendChild(buttonTwo);
-    mainSection.appendChild(answerTwo);
-    mainSection.appendChild(buttonThree);
-    mainSection.appendChild(answerThree);
-    mainSection.appendChild(buttonFour);
-    mainSection.appendChild(answerFour);
+    mainSection.append(buttonFive);
+    mainSection.append(answerOne);
+    mainSection.append(buttonSix);
+    mainSection.append(answerTwo);
+    mainSection.append(buttonSeven);
+    mainSection.append(answerThree);
+    mainSection.append(buttonEight);
+    mainSection.append(answerFour);
   
-    buttonOne.addEventListener("click", takeTime);
-    buttonTwo.addEventListener("click", addPoint);
-    buttonThree.addEventListener("click", takeTime);
-    buttonFour.addEventListener("click", takeTime);
+    buttonFive.addEventListener("click", takeTime);
+    buttonSix.addEventListener("click", addPoint);
+    buttonSeven.addEventListener("click", takeTime);
+    buttonEight.addEventListener("click", takeTime);
+    return score
+
+    
   
     function addPoint() {
       score = score + 10;
       console.log(score);
       startThree();
+      return score
     }
   
     function takeTime() {
       secondsLeft = secondsLeft - 10;
       startThree();
+      return score
     }
 }
 function startThree(){
     console.log("starting 3");
 
+    
+
     questionOne.textContent = "Which of the following is NOT truthy?";
+
+    buttonFive.remove();
+    buttonSix.remove();
+    buttonSeven.remove();
+    buttonEight.remove();
   
-    buttonOne.textContent = "A";
-    buttonTwo.textContent = "B";
-    buttonThree.textContent = "C";
-    buttonFour.textContent = "D";
+    button9.textContent = "A";
+    button10.textContent = "B";
+    button11.textContent = "C";
+    button12.textContent = "D";
   
     answerOne.textContent = "42";
     answerTwo.textContent = "[]";
@@ -143,40 +188,52 @@ function startThree(){
   
     mainSection.appendChild(questionOne);
   
-    mainSection.appendChild(buttonOne);
+    mainSection.appendChild(button9);
     mainSection.appendChild(answerOne);
-    mainSection.appendChild(buttonTwo);
+    mainSection.appendChild(button10);
     mainSection.appendChild(answerTwo);
-    mainSection.appendChild(buttonThree);
+    mainSection.appendChild(button11);
     mainSection.appendChild(answerThree);
-    mainSection.appendChild(buttonFour);
+    mainSection.appendChild(button12);
     mainSection.appendChild(answerFour);
   
-    buttonOne.addEventListener("click", takeTime);
-    buttonTwo.addEventListener("click", takeTime);
-    buttonThree.addEventListener("click", takeTime);
-    buttonFour.addEventListener("click", addPoint);
+    button9.addEventListener("click", takeTime);
+    button10.addEventListener("click", takeTime);
+    button11.addEventListener("click", takeTime);
+    button12.addEventListener("click", addPoint);
+    return score
+
+    
   
     function addPoint() {
       score = score + 10;
       console.log(score);
       startFour();
+      return score
     }
   
     function takeTime() {
       secondsLeft = secondsLeft - 10;
       startFour();
+      return score
     }
 }
 function startFour() {
     console.log("starting4");
 
+    
+
     questionOne.textContent = "Which of the following is NOT a sematic HTML tag?";
   
-    buttonOne.textContent = "A";
-    buttonTwo.textContent = "B";
-    buttonThree.textContent = "C";
-    buttonFour.textContent = "D";
+    button9.remove();
+    button10.remove();
+    button11.remove();
+    button12.remove();
+
+    button13.textContent = "A";
+    button14.textContent = "B";
+    button15.textContent = "C";
+    button16.textContent = "D";
   
     answerOne.textContent = "div";
     answerTwo.textContent = "aside";
@@ -185,40 +242,53 @@ function startFour() {
   
     mainSection.appendChild(questionOne);
   
-    mainSection.appendChild(buttonOne);
+    mainSection.appendChild(button13);
     mainSection.appendChild(answerOne);
-    mainSection.appendChild(buttonTwo);
+    mainSection.appendChild(button14);
     mainSection.appendChild(answerTwo);
-    mainSection.appendChild(buttonThree);
+    mainSection.appendChild(button15);
     mainSection.appendChild(answerThree);
-    mainSection.appendChild(buttonFour);
+    mainSection.appendChild(button16);
     mainSection.appendChild(answerFour);
   
-    buttonOne.addEventListener("click", addPoint);
-    buttonTwo.addEventListener("click", takeTime);
-    buttonThree.addEventListener("click", takeTime);
-    buttonFour.addEventListener("click", takeTime);
+    button13.addEventListener("click", addPoint);
+    button14.addEventListener("click", takeTime);
+    button15.addEventListener("click", takeTime);
+    button16.addEventListener("click", takeTime);
+    return score
+
+    
   
     function addPoint() {
       score = score + 10;
       console.log(score);
       startFive();
+      return score
     }
   
     function takeTime() {
       secondsLeft = secondsLeft - 10;
       startFive();
+      return score
     }
 }
 function startFive() {
     console.log("starting5");
 
+    
+    
+
     questionOne.textContent = "Which of the following can be stored in an array?";
   
-    buttonOne.textContent = "A";
-    buttonTwo.textContent = "B";
-    buttonThree.textContent = "C";
-    buttonFour.textContent = "D";
+    button13.remove();
+    button14.remove();
+    button15.remove();
+    button16.remove();
+
+    button17.textContent = "A";
+    button18.textContent = "B";
+    button19.textContent = "C";
+    button20.textContent = "D";
   
     answerOne.textContent = "numbers";
     answerTwo.textContent = "strings";
@@ -227,36 +297,41 @@ function startFive() {
   
     mainSection.appendChild(questionOne);
   
-    mainSection.appendChild(buttonOne);
+    mainSection.appendChild(button17);
     mainSection.appendChild(answerOne);
-    mainSection.appendChild(buttonTwo);
+    mainSection.appendChild(button18);
     mainSection.appendChild(answerTwo);
-    mainSection.appendChild(buttonThree);
+    mainSection.appendChild(button19);
     mainSection.appendChild(answerThree);
-    mainSection.appendChild(buttonFour);
+    mainSection.appendChild(button20);
     mainSection.appendChild(answerFour);
   
-    buttonOne.addEventListener("click", takeTime);
-    buttonTwo.addEventListener("click", takeTime);
-    buttonThree.addEventListener("click", takeTime);
-    buttonFour.addEventListener("click", addPoint);
+    button17.addEventListener("click", takeTime);
+    button18.addEventListener("click", takeTime);
+    button19.addEventListener("click", takeTime);
+    button20.addEventListener("click", addPoint);
+    return score
+
+    
   
     function addPoint() {
       score = score + 10;
       console.log(score);
       enterScore();
+      return score
     }
   
     function takeTime() {
       secondsLeft = secondsLeft - 10;
       enterScore();
+      return score
     }
 }
 
-function enterScore() {
-  console.log("enterscore!");
-  window.location.href = "high-scores.html";
-}
+// function enterScore() {
+//   console.log("enterscore!");
+//   window.location.href = "high-scores.html";
+// }
 
 startBtn.addEventListener("click", startDisappear);
 
