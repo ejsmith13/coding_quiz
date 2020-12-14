00aa3320e3d9d125b0c7c06088476892b16e386a
ejsmith13 <ejsmithwork@gmail.com>
2020-12-14 06:45:55 -0500

    local storage list function

--- highscore.js
+++ highscore.js
@@ -3,6 +3,25 @@ var nameBtn = document.querySelector("#scoreSubmit");
 var scoreName = document.querySelector("#scoreName");
 var scores = document.querySelector(".scoreList ");
 var clear = document.querySelector(".clear");
+function list() {
+  //object to store name and score of user
+  var user = {
+    firstName: scoreName.value.trim(),
+    score: localStorage.getItem("count"),
+  }; 
+  //creates list item for highscore list
+  var name = document.createElement("li");//set variable to get info out of local storage
+  var postedName = JSON.parse(localStorage.getItem("user"));
+
+  //giving content to list items
+  name.textContent = postedName.firstName + " " + postedName.score;
+
+  if (postedName.firstName != "") {
+    //adding name to highscore list
+    scores.append(name);
+  }
+}
+
 
 function replay() {
   window.location.href = "index.html";
@@ -18,15 +37,18 @@ function scoreList() {
   var user = {
     firstName: scoreName.value.trim(),
     score: localStorage.getItem("count"),
-  };
+  }; 
   //creates list item for highscore list
   var name = document.createElement("li");
   //storing user object in local storage
   localStorage.setItem("user", JSON.stringify(user));
+
   //set variable to get info out of local storage
   var postedName = JSON.parse(localStorage.getItem("user"));
+
   //giving content to list items
   name.textContent = postedName.firstName + " " + postedName.score;
+
 
   //adding name to highscore list
   scores.append(name);
@@ -34,8 +56,7 @@ function scoreList() {
   nameBtn.remove();
 }
 
- 
-
+list ();
 clear.addEventListener("click", clearNames);
 returnBtn.addEventListener("click", replay);
 nameBtn.addEventListener("click", scoreList);
