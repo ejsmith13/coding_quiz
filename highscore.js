ea59e018672f22d6e33fc122780391fa0ea7c64d
ejsmith13 <ejsmithwork@gmail.com>
2020-12-13 22:05:35 -0500

    reverting files

--- highscore.js
+++ highscore.js
@@ -34,21 +34,6 @@ function scoreList() {
   nameBtn.remove();
 }
 
-var user = {
-  firstName: scoreName.value.trim(),
-  score: localStorage.getItem("count"),
-};
-//creates list item for highscore list
-var name1 = document.createElement("li");
-//storing user object in local storage
-localStorage.setItem("user", JSON.stringify(user));
-//set variable to get info out of local storage
-var postedName = JSON.parse(localStorage.getItem("user"));
-//giving content to list items
-name1.textContent = postedName.firstName + " " + postedName.score;
-
-//adding name to highscore list
-scores.append(name1);
  
 
 clear.addEventListener("click", clearNames);
