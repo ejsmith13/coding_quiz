29a92d2806b01265d37811d4acbff7489ff8e6c8
ejsmith13 <ejsmithwork@gmail.com>
2020-12-13 21:08:57 -0500

    clear button

--- highscore.js
+++ highscore.js
@@ -2,31 +2,40 @@ var returnBtn = document.querySelector(".return");
 var nameBtn = document.querySelector("#scoreSubmit");
 var scoreName = document.querySelector("#scoreName");
 var scores = document.querySelector(".scoreList ");
+var clear = document.querySelector(".clear");
 
 function replay() {
   window.location.href = "index.html";
 }
 
-function scoreList() {
+function clearNames () {
+  localStorage.clear();
+}
 
+function scoreList() {
+  var user = {
+    firstName: scoreName.value.trim(),
+    score: localStorage.getItem("count"),
+  };
 
   //creating a list item to add to the high scores list.
   var name = document.createElement("li");
   // name submited through input
-  var highName = scoreName.value;
+  // var highName = scoreName.value;
   //storing names
-  localStorage.setItem("scorers", JSON.stringify(highName));
+  localStorage.setItem("user", JSON.stringify(user));
   //gets name from storage
-  var postedName = JSON.parse(localStorage.getItem("scorers"));
+  var postedName = JSON.parse(localStorage.getItem("user"));
   //gets score from game
-  var highscores = localStorage.getItem("count");
+  // var highscores = localStorage.getItem("count");
 
   //taking the name entered into the input field and assigning it to the list item.
-  name.textContent = postedName + " " + highscores;
+  name.textContent = postedName.firstName + " " + postedName.score;
 
   //adding name to highscore list
   scores.append(name);
 }
 
+clear.addEventListener("click", clearNames)
 returnBtn.addEventListener("click", replay);
 nameBtn.addEventListener("click", scoreList);
