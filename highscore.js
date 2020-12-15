b71ec6465e5453f0cfd407f447af5ac933ff4600
ejsmith13 <ejsmithwork@gmail.com>
2020-12-14 21:07:52 -0500

    fix highscore list appending

--- highscore.js
+++ highscore.js
@@ -3,25 +3,28 @@ var nameBtn = document.querySelector("#scoreSubmit");
 var scoreName = document.querySelector("#scoreName");
 var scores = document.querySelector(".scoreList ");
 var clear = document.querySelector(".clear");
+
 function list() {
-  //object to store name and score of user
+  // //object to store name and score of user
   var user = {
     firstName: scoreName.value.trim(),
     score: localStorage.getItem("count"),
-  }; 
+  };
   //creates list item for highscore list
-  var name = document.createElement("li");//set variable to get info out of local storage
-  var postedName = JSON.parse(localStorage.getItem("user"));
+  var name = document.createElement("li");
 
+  //set variable to get info out of local storage
+  var postedName = JSON.parse(localStorage.getItem("user"));
+  //if statement stops the function if there hasn't been a user yet
+  if (postedName === null) {
+    return;
+  }
   //giving content to list items
   name.textContent = postedName.firstName + " " + postedName.score;
 
-  if (postedName.firstName != "") {
-    //adding name to highscore list
-    scores.append(name);
-  }
+  //adding name to highscore list
+  scores.append(name);
 }
-
 
 function replay() {
   window.location.href = "index.html";
@@ -37,7 +40,7 @@ function scoreList() {
   var user = {
     firstName: scoreName.value.trim(),
     score: localStorage.getItem("count"),
-  }; 
+  };
   //creates list item for highscore list
   var name = document.createElement("li");
   //storing user object in local storage
@@ -49,14 +52,13 @@ function scoreList() {
   //giving content to list items
   name.textContent = postedName.firstName + " " + postedName.score;
 
-
   //adding name to highscore list
   scores.append(name);
   scoreName.remove();
   nameBtn.remove();
 }
 
-list ();
+list();
 clear.addEventListener("click", clearNames);
 returnBtn.addEventListener("click", replay);
 nameBtn.addEventListener("click", scoreList);
